64849503f0f8c391c390390071a9c1006c9484cd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 11:22:52 +0300

    Wrote the basic markup for the TaskDisplay app

--- src/Components/TaskDisplay.tsx
+++ src/Components/TaskDisplay.tsx
@@ -1,10 +1,35 @@
+'use client'
 import { useAppSelector } from '@/Redux/hooks'
-import { Card, CardBody, Flex, Text } from '@chakra-ui/react'
+import { Card, CardBody, Flex, Text, Divider } from '@chakra-ui/react'
 
 const TaskDisplay = () => {
 	const { tasks } = useAppSelector((state) => state.tasks)
 
-	return <Flex backdropBlur={'lg'} bgColor={'slateblue'}></Flex>
+	return (
+		<Flex
+			bgColor={'gray.100'}
+			justify={'center'}
+			align={'center'}
+			height={'100vh'}
+			width={'100vw'}
+		>
+			<Flex
+				padding={'2rem'}
+				bgColor={'slategray'}
+				borderRadius={'1rem'}
+				width={'50%'}
+				height={'75%'}
+				align={'center'}
+				color={'white'}
+				direction={'column'}
+			>
+				<Text paddingBottom={'0.5rem'} fontSize={'1.5rem'}>
+					Testing
+				</Text>
+				<Divider />
+			</Flex>
+		</Flex>
+	)
 }
 
 export default TaskDisplay
