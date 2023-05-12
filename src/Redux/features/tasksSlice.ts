1310c6e59ed3c2b1227d83b30838b35088f8d4a5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 09:58:56 +0300

    Defined the tasksSlice

--- src/Redux/features/tasksSlice.ts
+++ src/Redux/features/tasksSlice.ts
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import build from 'next/dist/build'
 
 interface Task {
 	task_content: string
@@ -7,10 +8,16 @@ interface Task {
 	is_loading: boolean
 }
 
-const initialState: Task
-{
-	task_content: ''
-	is_complete: false
-	date_created: Date.now()
+const initialState: Task = {
+	task_content: '',
+	is_complete: false,
+	date_created: new Date(),
 	is_loading: false
 }
+
+const tasksSlice = createSlice({
+	name: 'task',
+	initialState,
+	reducers: {},
+	extraReducers: (builder) => {}
+})
