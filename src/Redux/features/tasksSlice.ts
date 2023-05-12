7903626f5e5eddb83cb228a96d96f345794c88d5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 10:34:34 +0300

    Defined the proper logic for the fulfilled state of tge getAllTasjs async thunk

--- src/Redux/features/tasksSlice.ts
+++ src/Redux/features/tasksSlice.ts
@@ -1,7 +1,10 @@
 import axios from 'axios'
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 
-const getAllTasks = createAsyncThunk('tasks/getAllTasks', async () => {})
+const getAllTasks = createAsyncThunk('tasks/getAllTasks', async () => {
+	const tasks = await axios.get('http://localhost:8000/tasks')
+	return tasks.data
+})
 
 interface Task {
 	task_content: string
@@ -21,7 +24,24 @@ const tasksSlice = createSlice({
 	name: 'task',
 	initialState,
 	reducers: {},
-	extraReducers: (builder) => {}
+	extraReducers: (builder) => {
+		builder
+			.addCase(getAllTasks.pending, (state) => {
+				state.is_loading = true
+			})
+			.addCase(
+				getAllTasks.fulfilled,
+				(state, action: PayloadAction<Task>) => {
+					state.is_loading = false
+					state.task_content = action.payload.task_content
+					state.is_complete = action.payload.is_complete
+					state.date_created = action.payload.date_created
+				}
+			)
+			.addCase(getAllTasks.rejected, (state) => {
+				state.is_loading = false
+			})
+	}
 })
 
 export default tasksSlice.reducer
