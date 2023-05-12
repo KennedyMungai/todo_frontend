2cda6628e35e2abcdb980fd692c14f8f980c991e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 10:02:28 +0300

    Created the TaskDIsplay components

---
+++ src/Components/SingleTaskDisplay.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const SingleTaskDisplay = (props: Props) => {
+	return <div>SingleTaskDisplay</div>
+}
+
+export default SingleTaskDisplay
---
+++ src/Components/TaskDisplay.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const TaskDisplay = (props: Props) => {
+  return (
+    <div>TaskDisplay</div>
+  )
+}
+
+export default TaskDisplay
