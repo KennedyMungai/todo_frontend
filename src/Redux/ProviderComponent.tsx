ad639634155ccfee668846ae61731db040a7fded
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 09:49:45 +0300

    Created the Redux Provider coponent

---
+++ src/Redux/ProviderComponent.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+import store from './store'
+
+type Props = {
+	children: ReactNode
+}
+
+const ProviderComponent = ({ children }: Props) => {
+	return <Provider store={store}>{children}</Provider>
+}
+
+export default ProviderComponent
