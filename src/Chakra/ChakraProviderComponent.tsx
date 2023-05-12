76ee04713a1c1bea75935bfc78d52748d169d205
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 09:39:52 +0300

    Imported the theme file

--- src/Chakra/ChakraProviderComponent.tsx
+++ src/Chakra/ChakraProviderComponent.tsx
@@ -1,11 +1,13 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { ReactNode } from 'react'
+import theme from './theme'
 
 type Props = {
 	children: ReactNode
 }
 
 const ChakraProviderComponent = ({ children }: Props) => {
-	return <div>{children}</div>
+	return <ChakraProvider theme={theme}>{children}</ChakraProvider>
 }
 
 export default ChakraProviderComponent
