acdf6424045e6f9fe928bda69e3e0e4f25a234c8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-12 09:55:45 +0300

    Created the initialState object

--- src/Redux/features/tasksSlice.ts
+++ src/Redux/features/tasksSlice.ts
@@ -3,6 +3,14 @@ import { createSlice } from '@reduxjs/toolkit'
 interface Task {
 	task_content: string
 	is_complete: boolean
-	date_created: string
+	date_created: Date
 	is_loading: boolean
+}
+
+const initialState: Task
+{
+	task_content: ''
+	is_complete: false
+	date_created: Date.now()
+	is_loading: false
 }
